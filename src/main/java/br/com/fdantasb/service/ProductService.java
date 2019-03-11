631e37290b66fc880467d581baf800a80106806a
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 16:38:10 -0300

    Criada injecao do repositorio de produtos

--- src/main/java/br/com/fdantasb/service/ProductService.java
+++ src/main/java/br/com/fdantasb/service/ProductService.java
@@ -1,4 +1,12 @@
 package br.com.fdantasb.service;
 
+import br.com.fdantasb.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
 }

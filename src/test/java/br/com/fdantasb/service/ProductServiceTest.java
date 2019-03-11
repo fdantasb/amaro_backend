674ab8150d127294ee6ba228184a0b21ff2f87bc
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 18:29:25 -0300

    Implementação de testes da classe productservice

--- src/test/java/br/com/fdantasb/service/ProductServiceTest.java
+++ src/test/java/br/com/fdantasb/service/ProductServiceTest.java
@@ -1,21 +1,47 @@
 package br.com.fdantasb.service;
 
+import br.com.fdantasb.model.Product;
+import br.com.fdantasb.model.Tag;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProductServiceTest {
 
-    @Inject
+    @Autowired
     private ProductService productService;
+
+    @Test
+    public void findTagSuccess(){
+        Tag tag = productService.findTagByName("balada");
+        Assert.assertNotNull(tag);
+    }
 
     @Test
     public void createProductSuccess(){
 
+        Product product = new Product();
+        product.setId(58345l);
+        product.setName("Saia Maxi Chiffon Saint");
 
-        //productService.createProduct();
+        List<String> taglist = new ArrayList<>();
+        taglist.addAll(Arrays.asList("balada", "metal", "delicado", "descolado"));
+
+        product.setLabelTagList(taglist);
+
+        Product result = productService.createProduct(product);
+
+        Assert.assertNotNull(result);
+
     }
 
 }

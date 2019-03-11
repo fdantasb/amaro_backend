43271451dec1b47ad55411b05d894b29d1ded9c3
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 20:59:08 -0300

    Adicionados os atributos na classe de erro

--- src/main/java/br/com/fdantasb/model/ErrorDetails.java
+++ src/main/java/br/com/fdantasb/model/ErrorDetails.java
@@ -1,4 +1,32 @@
 package br.com.fdantasb.model;
 
 public class ErrorDetails {
+
+    private String title;
+    private Long status;
+    private Long timestamp;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getStatus() {
+        return status;
+    }
+
+    public void setStatus(Long status) {
+        this.status = status;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
 }

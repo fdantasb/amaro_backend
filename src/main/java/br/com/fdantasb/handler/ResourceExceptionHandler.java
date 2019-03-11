23da7c8295eec531b22dd26dd14b66c8a26c2aec
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 20:59:30 -0300

    Adicionado Handler de produto nao encontrado

--- src/main/java/br/com/fdantasb/handler/ResourceExceptionHandler.java
+++ src/main/java/br/com/fdantasb/handler/ResourceExceptionHandler.java
@@ -1,4 +1,24 @@
 package br.com.fdantasb.handler;
 
+import br.com.fdantasb.model.ErrorDetails;
+import br.com.fdantasb.service.exception.ProductException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
 public class ResourceExceptionHandler {
+    @ExceptionHandler(ProductException.class)
+    public ResponseEntity<ErrorDetails> ProductException(MethodArgumentNotValidException ex, HttpServletRequest request){
+        ErrorDetails result = new ErrorDetails();
+        result.setTitle(ex.getMessage());
+        result.setStatus(404l);
+        result.setTimestamp(System.currentTimeMillis());
+
+        ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+    }
 }

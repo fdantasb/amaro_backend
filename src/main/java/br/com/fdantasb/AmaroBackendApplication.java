bf60ec1d71931f34eb593378ca2ee7baf393a289
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 13:45:47 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/fdantasb/AmaroBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fdantasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fdantasb/AmaroBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fdantasb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmaroBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

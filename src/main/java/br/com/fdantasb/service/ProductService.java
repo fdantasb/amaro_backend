bcaeeea39ba168376fa64b665b38c7889b926444
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 18:29:41 -0300

    Adicionadas melhorias na classe

--- src/main/java/br/com/fdantasb/service/ProductService.java
+++ src/main/java/br/com/fdantasb/service/ProductService.java
@@ -1,12 +1,62 @@
 package br.com.fdantasb.service;
 
+import br.com.fdantasb.model.Product;
+import br.com.fdantasb.model.Tag;
 import br.com.fdantasb.repository.ProductRepository;
+import br.com.fdantasb.repository.TagRepository;
+import br.com.fdantasb.service.exception.ProductException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
 
+    public static final String EXISTENT_PRODUCT = "Produto já existe.";
+    public static final String TAG_NAO_ENCONTRADA = "Tag não Encontrada.";
+    private static Logger LOG = LoggerFactory.getLogger(ProductService.class);
+
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    public Tag findTagByName(String string) {
+        Tag result = tagRepository.findByNome(string);
+        return result;
+    }
+
+    public Product createProduct(Product product) {
+        if (productExist(product)){
+            LOG.info(EXISTENT_PRODUCT);
+            throw new ProductException(EXISTENT_PRODUCT);
+        }
+        verifyTagList(product.getLabelTagList());
+
+        Product result = productRepository.save(product);
+        LOG.info("Produto salvo id: " + product.getId());
+
+        return result;
+    }
+
+    private void verifyTagList(List<String> list) {
+        List<String> strings = list.stream().filter(s -> findTagByName(s) == null).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(strings)) {
+            LOG.info("As seguintes Tags não foram encontradas:\n");
+            strings.stream().forEach(s -> LOG.info(s));
+            throw new ProductException(TAG_NAO_ENCONTRADA);
+        }
+    }
+
+    private boolean productExist(Product product) {
+        Optional<Product> result = productRepository.findById(product.getId());
+        return result.isPresent();
+    }
 }

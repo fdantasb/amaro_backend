294bad8578fc00326c68badc12d0d248c52ab295
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 17:18:26 -0300

    Inicio de implementacao de classe de teste

--- src/main/java/br/com/fdantasb/repository/TagRepository.java
+++ src/main/java/br/com/fdantasb/repository/TagRepository.java
@@ -1,4 +1,7 @@
 package br.com.fdantasb.repository;
 
-public interface TagRepository {
+import br.com.fdantasb.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
 }
--- src/test/java/br/com/fdantasb/service/ProductServiceTest.java
+++ src/test/java/br/com/fdantasb/service/ProductServiceTest.java
@@ -1,5 +1,21 @@
+package br.com.fdantasb.service;
+
+import org.junit.Test;
+
+import javax.inject.Inject;
+
 import static org.junit.Assert.*;
 
 public class ProductServiceTest {
+
+    @Inject
+    private ProductService productService;
+
+    @Test
+    public void createProductSuccess(){
+
+
+        //productService.createProduct();
+    }
 
 }

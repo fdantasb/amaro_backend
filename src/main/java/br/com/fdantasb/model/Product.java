a68d54c62a3cbb907fa899dd4be7b7f7e9687ed0
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 15:21:43 -0300

    Adicionados os atributos as classes e criado o repositorio de produtos

--- src/main/java/br/com/fdantasb/model/Product.java
+++ src/main/java/br/com/fdantasb/model/Product.java
@@ -1,6 +1,48 @@
 package br.com.fdantasb.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonInclude(Include.NON_NULL)
+    private Long id;
+
+    @NotEmpty(message = "O campo nome não pode ser vazio.")
+    private String name;
+    @JsonIgnore
+    @ManyToMany
+    private List<Tag> tagList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Tag> getTagList() {
+        return tagList;
+    }
+
+    public void setTagList(List<Tag> tagList) {
+        this.tagList = tagList;
+    }
 }
--- src/main/java/br/com/fdantasb/model/Tag.java
+++ src/main/java/br/com/fdantasb/model/Tag.java
@@ -1,4 +1,47 @@
 package br.com.fdantasb.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Entity
 public class Tag {
+    @Id
+    @JsonInclude(Include.NON_NULL)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "product_tag")
+    private List<Product> productList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }

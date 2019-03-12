7d2f6e9feaace105e37860a606d91d9e54b1c8ab
Flavio Dantas <fdantasb@gmail.com>
2019-03-11 21:07:35 -0300

    Criado o Handler de tag não encontrada

--- src/main/java/br/com/fdantasb/handler/ResourceExceptionHandler.java
+++ src/main/java/br/com/fdantasb/handler/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package br.com.fdantasb.handler;
 
 import br.com.fdantasb.model.ErrorDetails;
 import br.com.fdantasb.service.exception.ProductNotFoundException;
+import br.com.fdantasb.service.exception.TagNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,13 +13,23 @@ import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<ErrorDetails> ProductException(MethodArgumentNotValidException ex, HttpServletRequest request){
+    public ResponseEntity<ErrorDetails> productNotFoundException(ProductNotFoundException ex, HttpServletRequest request){
         ErrorDetails result = new ErrorDetails();
         result.setTitle(ex.getMessage());
         result.setStatus(404l);
         result.setTimestamp(System.currentTimeMillis());
 
-        ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+    }
+
+    public ResponseEntity<ErrorDetails> tagNotFoundException(TagNotFoundException ex, HttpServletRequest request){
+        ErrorDetails result = new ErrorDetails();
+        result.setTitle(ex.getMessage());
+        result.setStatus(404l);
+        result.setTimestamp(System.currentTimeMillis());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
     }
 }

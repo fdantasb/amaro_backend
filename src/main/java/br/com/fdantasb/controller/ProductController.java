5f6e894eb8de068e037507257a18d9a80d569c2c
Flavio Dantas <fdantasb@gmail.com>
2019-03-14 12:18:19 -0300

    Ajustes no metodo de criação de produtos

--- src/main/java/br/com/fdantasb/controller/ProductController.java
+++ src/main/java/br/com/fdantasb/controller/ProductController.java
@@ -1,14 +1,31 @@
 package br.com.fdantasb.controller;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.fdantasb.model.Product;
+import br.com.fdantasb.service.ProductService;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+	@Autowired
+	private ProductService productService;
 	
-	@RequestMapping("/hello")
-	public String hello() {
-		return "Hello World!";
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ResponseEntity<Void> save (@RequestBody Product product)	{
+		product = productService.createProduct(product);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }

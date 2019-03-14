4c8e9cc0d3eb0d6ff5223928a84d1de3073709e3
Flavio Dantas <fdantasb@gmail.com>
2019-03-14 12:49:21 -0300

    removido o gerador de id

--- src/main/java/br/com/fdantasb/model/Product.java
+++ src/main/java/br/com/fdantasb/model/Product.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Transient;
@@ -21,7 +19,7 @@ public class Product implements Serializable{
 	private static final long serialVersionUID = 2058550393627193515L;
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     @JsonInclude(Include.NON_NULL)
     private Long id;
 

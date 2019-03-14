43a47b800f5e6d54be4f00d6fd976976ef79a240
Flavio Dantas <fdantasb@gmail.com>
2019-03-14 12:49:01 -0300

    implementado o serial

--- src/main/java/br/com/fdantasb/service/exception/ProductNotFoundException.java
+++ src/main/java/br/com/fdantasb/service/exception/ProductNotFoundException.java
@@ -2,9 +2,9 @@ package br.com.fdantasb.service.exception;
 
 public class ProductNotFoundException extends IllegalArgumentException {
 
+	private static final long serialVersionUID = 2693881392048944662L;
 
-
-    public ProductNotFoundException(String message, Throwable cause) {
+	public ProductNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
 
--- src/main/java/br/com/fdantasb/service/exception/TagNotFoundException.java
+++ src/main/java/br/com/fdantasb/service/exception/TagNotFoundException.java
@@ -2,7 +2,9 @@ package br.com.fdantasb.service.exception;
 
 public class TagNotFoundException extends IllegalArgumentException{
 
-    public TagNotFoundException(String message, Throwable cause){
+	private static final long serialVersionUID = 1239218084169404768L;
+
+	public TagNotFoundException(String message, Throwable cause){
         super(message, cause);
     }
 

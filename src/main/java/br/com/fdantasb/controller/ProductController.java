ff523599d8738aa64cad2506ab1bcb5fde920f15
Flavio Dantas <fdantasb@gmail.com>
2019-03-14 16:15:34 -0300

    Ajuste para o novo retorno no json

--- src/main/java/br/com/fdantasb/controller/ProductController.java
+++ src/main/java/br/com/fdantasb/controller/ProductController.java
@@ -1,5 +1,7 @@
 package br.com.fdantasb.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.fdantasb.data.ProductList;
+import br.com.fdantasb.data.ProductData;
+import br.com.fdantasb.data.ProductListReceive;
 import br.com.fdantasb.service.ProductService;
 
 @RestController
@@ -19,8 +22,14 @@ public class ProductController {
 	private ProductService productService;
 	
 	@RequestMapping(value = "/create/list", method = RequestMethod.POST)
-	public ResponseEntity<Void> save (@RequestBody ProductList products)	{
+	public ResponseEntity<Void> saveList(@RequestBody ProductListReceive products)	{
 		productService.createProductList(products.getProducts());
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ProductData>> getList()	{
+		List<ProductData> result = productService.findAllProductDataList();
+		return ResponseEntity.ok().body(result);
 	}
 }
